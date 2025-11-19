5a067ea0cd57e9d21ce297808f139af3218cc2a3
Enio Enrique <84923225+dryeniio@users.noreply.github.com>
2025-11-19 20:43:21 -0300

    19.11.25 - 2

--- POO/19.11.2025/main.py
+++ POO/19.11.2025/main.py
@@ -1,4 +1,6 @@
 import random
+#Enio Enrique - 01851994
+#Atividade 05 - Listas
 
 #Questão 1
 lista = list(range(1, 6)) 
@@ -9,4 +11,10 @@ print(lista[0],lista[len(lista)//2],lista[-1])
 
 #Questão 3
 lista[random.randint(0,len(lista)-1)] = 27
+print(lista)
+
+#Questão 4
+# Se eu adicionar dois elemento: lista.extend([6,7])
+lista.append(6)
+lista.append(7)
 print(lista)
